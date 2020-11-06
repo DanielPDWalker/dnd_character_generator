7fcbec334e91ea1c2e609c4e7d620c0d812fc8a6
danielpdwalker <danielpdwalker@gmail.com>
2020-11-06 18:25:33 +0000

    Added character alignment print outs and alignment_roll func

--- dnd_stat_rolls.py
+++ dnd_stat_rolls.py
@@ -13,11 +13,12 @@ with open('data/races.csv', 'r') as file:
     for item in file:
         RACES.append(item)
 
+
 def character_creation_rolls():
     roll_holding_list = []
     stat_holding_list = []
-    for i in  range(1,7):
-        for i in range(1,5):
+    for i in range(1, 7):
+        for i in range(1, 5):
             roll_holding_list.append(r.randint(1, 6))
         roll_holding_list.sort(reverse=True)
         roll_holding_list.pop()
@@ -28,11 +29,18 @@ def character_creation_rolls():
     stat_holding_list.sort(reverse=True)
     return stat_holding_list
 
+
 def character_class_roll():
     return str(r.choice(CLASSES)).strip()
 
+
 def character_race_roll():
     return str(r.choice(RACES)).strip()
+
+
+def character_alignment_roll():
+    pass
+
 
 def print_character(character=None):
     if character == None:
@@ -43,7 +51,8 @@ def print_character(character=None):
         print('-------------')
         character_race = character_race_roll()
         character_class = character_class_roll()
-        print(character_race + ' - ' + character_class)      
+        print(character_alignment)
+        print(character_race + ' - ' + character_class)
     else:
         for stat in character:
             print(stat)
@@ -51,12 +60,14 @@ def print_character(character=None):
         print('-------------')
         character_race = character_race_roll()
         character_class = character_class_roll()
+        print(character_alignment)
         print(character_race + ' - ' + character_class)
+
 
 def generate_one_good_one_bad_character():
     counter = 0
     best_character = []
-    worst_character = [18, 18, 18, 18, 18 ,18]
+    worst_character = [18, 18, 18, 18, 18, 18]
     while counter < 10000:
         counter += 1
         character_stats = character_creation_rolls()
@@ -69,6 +80,7 @@ def generate_one_good_one_bad_character():
     print_character(best_character)
     print('Worst Character')
     print_character(worst_character)
+
 
 if __name__ == "__main__":
     print_character()
